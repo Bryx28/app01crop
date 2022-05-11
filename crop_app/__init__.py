d2f448790c2b3d23688c93c3decbb4a667586277
Bryx28 <qjbfquebral@tip.edu.ph>
2022-05-11 09:13:16 +0800

    Added environment variables

--- crop_app/__init__.py
+++ crop_app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from decouple import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '095113c2cb7c7asvde59a67b061db9a'
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
